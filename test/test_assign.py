12e8b9831fe00ef8bf3937d5025743922e807ad3
George Hotz <72895+geohot@users.noreply.github.com>
2024-03-22 11:20:06 -0700

    preschedule all (#3875)

--- test/test_assign.py
+++ test/test_assign.py
@@ -115,6 +115,14 @@ class TestAssign(unittest.TestCase):
     new = a + (times_a-1).contiguous()
     np.testing.assert_allclose(new.numpy(), 4)
 
+  @unittest.expectedFailure
+  def test_assign_diamond_possible(self):
+    a = Tensor.ones(4).contiguous().realize()
+    times_a = a*3
+    a.assign(Tensor.full((4,), 2.).contiguous())
+    new = a + (times_a+1).contiguous()
+    np.testing.assert_allclose(new.numpy(), 6)
+
   def test_assign_diamond_alt(self):
     a = Tensor.ones(4).contiguous().realize()
     a.assign(Tensor.full((4,), 2.).contiguous())
